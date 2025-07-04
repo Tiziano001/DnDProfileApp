d6d3547c6e12e37280dbf76c7658193c64f181e9
Tiziano <tizianoattivissimo@live.it>
2025-07-04 18:35:32 +0200

    Add files via upload

---
+++ DnDProfileApp/DnDProfileApp/Form1.Designer.cs
@@ -0,0 +1,511 @@
+﻿namespace DnDProfileApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
+            textBox10 = new TextBox();
+            textBox11 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            textBox12 = new TextBox();
+            textBox13 = new TextBox();
+            label12 = new Label();
+            label13 = new Label();
+            textBox14 = new TextBox();
+            label14 = new Label();
+            textBox15 = new TextBox();
+            label15 = new Label();
+            label16 = new Label();
+            textBox16 = new TextBox();
+            listBox1 = new ListBox();
+            label17 = new Label();
+            listBox2 = new ListBox();
+            label18 = new Label();
+            label19 = new Label();
+            listBox3 = new ListBox();
+            label20 = new Label();
+            listBox4 = new ListBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(39, 12);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(215, 54);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(359, 12);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(490, 12);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(621, 12);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 27);
+            textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(752, 12);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(125, 27);
+            textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(39, 99);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(48, 27);
+            textBox6.TabIndex = 5;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(160, 99);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(48, 27);
+            textBox7.TabIndex = 6;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(281, 99);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(48, 27);
+            textBox8.TabIndex = 7;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(405, 99);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(48, 27);
+            textBox9.TabIndex = 8;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(531, 99);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(48, 27);
+            textBox10.TabIndex = 9;
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(657, 99);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(48, 27);
+            textBox11.TabIndex = 10;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(39, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 11;
+            label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(359, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(109, 20);
+            label2.TabIndex = 12;
+            label2.Text = "Classe e Livello";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(490, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(88, 20);
+            label3.TabIndex = 13;
+            label3.Text = "Background";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(621, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(48, 20);
+            label4.TabIndex = 14;
+            label4.Text = "Razza";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(752, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(74, 20);
+            label5.TabIndex = 15;
+            label5.Text = "Giocatore";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(37, 90);
+            label6.Name = "label6";
+            label6.Size = new Size(55, 20);
+            label6.TabIndex = 16;
+            label6.Text = "FORZA";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(141, 90);
+            label7.Name = "label7";
+            label7.Size = new Size(89, 20);
+            label7.TabIndex = 17;
+            label7.Text = "DESTREZZA";
+            label7.Click += label7_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(256, 90);
+            label8.Name = "label8";
+            label8.Size = new Size(110, 20);
+            label8.TabIndex = 18;
+            label8.Text = "COSTITUZIONE";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(378, 90);
+            label9.Name = "label9";
+            label9.Size = new Size(106, 20);
+            label9.TabIndex = 19;
+            label9.Text = "INTELLIGENZA";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(515, 90);
+            label10.Name = "label10";
+            label10.Size = new Size(83, 20);
+            label10.TabIndex = 20;
+            label10.Text = "SAGGEZZA";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(647, 90);
+            label11.Name = "label11";
+            label11.Size = new Size(72, 20);
+            label11.TabIndex = 21;
+            label11.Text = "CARISMA";
+            // 
+            // textBox12
+            // 
+            textBox12.Location = new Point(39, 498);
+            textBox12.Multiline = true;
+            textBox12.Name = "textBox12";
+            textBox12.Size = new Size(125, 50);
+            textBox12.TabIndex = 22;
+            // 
+            // textBox13
+            // 
+            textBox13.Location = new Point(227, 498);
+            textBox13.Multiline = true;
+            textBox13.Name = "textBox13";
+            textBox13.Size = new Size(125, 50);
+            textBox13.TabIndex = 23;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(41, 475);
+            label12.Name = "label12";
+            label12.Size = new Size(104, 20);
+            label12.TabIndex = 24;
+            label12.Text = "Saving Throws";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(227, 475);
+            label13.Name = "label13";
+            label13.Size = new Size(42, 20);
+            label13.TabIndex = 25;
+            label13.Text = "Skills";
+            // 
+            // textBox14
+            // 
+            textBox14.Location = new Point(39, 215);
+            textBox14.Name = "textBox14";
+            textBox14.Size = new Size(53, 27);
+            textBox14.TabIndex = 26;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(39, 192);
+            label14.Name = "label14";
+            label14.Size = new Size(28, 20);
+            label14.TabIndex = 27;
+            label14.Text = "HP";
+            // 
+            // textBox15
+            // 
+            textBox15.Location = new Point(141, 215);
+            textBox15.Name = "textBox15";
+            textBox15.Size = new Size(53, 27);
+            textBox15.TabIndex = 28;
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(141, 192);
+            label15.Name = "label15";
+            label15.Size = new Size(71, 20);
+            label15.TabIndex = 29;
+            label15.Text = "Armatura";
+            label15.Click += label15_Click;
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Location = new Point(246, 192);
+            label16.Name = "label16";
+            label16.Size = new Size(62, 20);
+            label16.TabIndex = 31;
+            label16.Text = "Velocità";
+            // 
+            // textBox16
+            // 
+            textBox16.Location = new Point(246, 215);
+            textBox16.Name = "textBox16";
+            textBox16.Size = new Size(53, 27);
+            textBox16.TabIndex = 30;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(765, 70);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(87, 104);
+            listBox1.TabIndex = 32;
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.Location = new Point(765, 56);
+            label17.Name = "label17";
+            label17.Size = new Size(53, 20);
+            label17.TabIndex = 33;
+            label17.Text = "Lingue";
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.Location = new Point(37, 308);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(262, 104);
+            listBox2.TabIndex = 34;
+            // 
+            // label18
+            // 
+            label18.AutoSize = true;
+            label18.Location = new Point(39, 295);
+            label18.Name = "label18";
+            label18.Size = new Size(93, 20);
+            label18.TabIndex = 35;
+            label18.Text = "INVENTARIO";
+            // 
+            // label19
+            // 
+            label19.AutoSize = true;
+            label19.Location = new Point(319, 295);
+            label19.Name = "label19";
+            label19.Size = new Size(115, 20);
+            label19.TabIndex = 37;
+            label19.Text = "E.INVOCATIONS";
+            label19.Click += label19_Click;
+            // 
+            // listBox3
+            // 
+            listBox3.FormattingEnabled = true;
+            listBox3.Location = new Point(317, 308);
+            listBox3.Name = "listBox3";
+            listBox3.Size = new Size(262, 104);
+            listBox3.TabIndex = 36;
+            // 
+            // label20
+            // 
+            label20.AutoSize = true;
+            label20.Location = new Point(592, 295);
+            label20.Name = "label20";
+            label20.Size = new Size(99, 20);
+            label20.TabIndex = 39;
+            label20.Text = "INCANTESIMI";
+            label20.Click += label20_Click;
+            // 
+            // listBox4
+            // 
+            listBox4.FormattingEnabled = true;
+            listBox4.Location = new Point(590, 308);
+            listBox4.Name = "listBox4";
+            listBox4.Size = new Size(262, 104);
+            listBox4.TabIndex = 38;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(719, 652);
+            button1.Name = "button1";
+            button1.Size = new Size(107, 50);
+            button1.TabIndex = 40;
+            button1.Text = "Salva";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(884, 739);
+            Controls.Add(button1);
+            Controls.Add(label20);
+            Controls.Add(listBox4);
+            Controls.Add(label19);
+            Controls.Add(listBox3);
+            Controls.Add(label18);
+            Controls.Add(listBox2);
+            Controls.Add(label17);
+            Controls.Add(listBox1);
+            Controls.Add(label16);
+            Controls.Add(textBox16);
+            Controls.Add(label15);
+            Controls.Add(textBox15);
+            Controls.Add(label14);
+            Controls.Add(textBox14);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(textBox13);
+            Controls.Add(textBox12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox11);
+            Controls.Add(textBox10);
+            Controls.Add(textBox9);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private TextBox textBox10;
+        private TextBox textBox11;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private TextBox textBox12;
+        private TextBox textBox13;
+        private Label label12;
+        private Label label13;
+        private TextBox textBox14;
+        private Label label14;
+        private TextBox textBox15;
+        private Label label15;
+        private Label label16;
+        private TextBox textBox16;
+        private ListBox listBox1;
+        private Label label17;
+        private ListBox listBox2;
+        private Label label18;
+        private Label label19;
+        private ListBox listBox3;
+        private Label label20;
+        private ListBox listBox4;
+        private Button button1;
+    }
+}
---
+++ DnDProfileApp/DnDProfileApp/Form1.cs
@@ -0,0 +1,40 @@
+namespace DnDProfileApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label19_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label20_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
